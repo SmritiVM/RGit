8af6535fcfc6a1690dc3bf3924ac3f688a430c16
Smriti Vipin Madangarli <smritivipin.madangarli2021@vitstudent.ac.in>
2024-12-26 14:33:43 +0530

    use paths from paths

--- wegit/src/components/add.rs
+++ wegit/src/components/add.rs
@@ -1,8 +1,7 @@
 use crate::structures::index::{Index}; 
 use crate::components::hash_and_compress;
+use crate::structures::paths;
 use std::path::Path;
-
-const INDEX_FILE_PATH: &str = ".wegit/index";
 
 pub fn add(filepath: &str) {
     let data = match std::fs::read(filepath) {
@@ -14,17 +13,15 @@ pub fn add(filepath: &str) {
     };
     let hash_code = hash_and_compress::calculate_sha1(&data); 
 
-    let mut index = match Index::read_index(INDEX_FILE_PATH) {
+    let mut index = match Index::read_index(paths::INDEX) {
         Ok(index) => index, 
         Err(_) => Index::new(),
     };
     index.add_index_object(filepath, &hash_code, "A"); // A to denote that the file has been added but not commited
-    if let Err(e) = index.write_index(INDEX_FILE_PATH) {
+    if let Err(e) = index.write_index(paths::INDEX) {
         eprintln!("Error writing to index file: {}", e);
         return; 
     }
     println!("File '{}' added.", filepath);
-
-    let objects_dir = Path::new(".wegit").join("file_objects");
-    hash_and_compress::create_object(&objects_dir, &data, &hash_code);
+    hash_and_compress::create_object(Path::new(paths::FILE_OBJECTS), &data, &hash_code);
 }
