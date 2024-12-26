55e0b5c858968472f8fda65c272bd1cef68e0fa7
Smriti Vipin Madangarli <smritivipin.madangarli2021@vitstudent.ac.in>
2024-12-26 17:00:55 +0530

    added index objects path

--- wegit/src/structures/paths.rs
+++ wegit/src/structures/paths.rs
@@ -2,3 +2,4 @@ pub const INDEX: &str = ".wegit/index";
 pub const HEAD: &str = ".wegit/HEAD";
 pub const COMMIT: &str = ".wegit/commit";
 pub const FILE_OBJECTS: &str = ".wegit/file_objects";
+pub const INDEX_OBJECTS: &str = ".wegit/index_objects";
